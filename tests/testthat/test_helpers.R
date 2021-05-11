951598028b8fcdf6089f3e132564bf47da40ccdd
Hubert Thieriot <hubert@energyandcleanair.org>
2021-05-11 17:43:52 +0800

    -make_tifs testing [wip]

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -12,7 +12,7 @@ test_that("get_grids_calpuff works", {
   for(example_dir in list.dirs(test_data_dir(), recursive = F)){
     print(example_dir)
     
-    # Get list of ciles
+    # Get list of files
     calpuff_files <- creapuff::get_calpuff_files(dir=example_dir)
     expect_gt(nrow(calpuff_files), 0)
   
@@ -27,6 +27,40 @@ test_that("get_grids_calpuff works", {
                                          utm_hem=utm_hem,
                                          map_res=10)
     expect_true(all(c("gridR","gridLL","gridSP") %in% names(grids)))
+  }
+})
+
+
+
+test_that("make_tifs works", {
+  
+  expect_true(dir.exists(test_data_dir()))
+  
+  for(example_dir in list.dirs(test_data_dir(), recursive = F)){
+    print(example_dir)
+    
+    # Remove preexisting tif files
+    tif_files <- list.files(example_dir, ".tif", full.names = T)
+    file.remove(tif_files)
+    
+    # Get list of files
+    calpuff_files <- creapuff::get_calpuff_files(dir=example_dir)
+    expect_gt(nrow(calpuff_files), 0)
+    
+    # Get UTM information from plants
+    plants <- read.csv(file.path(example_dir, "plants.csv"))
+    utm_zone <- get_utm_zone(plants)
+    utm_hem <- get_utm_hem(plants)
+    
+    # Get grids
+    grids <- creapuff::get_grids_calpuff(calpuff_files=calpuff_files,
+                                         utm_zone=utm_zone,
+                                         utm_hem=utm_hem,
+                                         map_res=10)
+    expect_true(all(c("gridR","gridLL","gridSP") %in% names(grids)))
+    
+    # Make tifs
+    creapuff::make_tifs(calpuff_files=calpuff_files, grids=grids)
   }
 })
 
