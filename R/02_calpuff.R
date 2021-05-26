2a4a93a4cd234e1d66a6b66b0677a0648b6167a0
laurimyllyvirta <35026118+laurimyllyvirta@users.noreply.github.com>
2021-05-26 06:50:51 +0000

    give set default emissions_data=NULL and skip emissions processing unless emissions_data is given

--- R/02_calpuff.R
+++ R/02_calpuff.R
@@ -16,8 +16,8 @@
 runCalpuff <- function(
   run_name,
   params_allgrids,
-  emissions_data, #data.frame with emissions data for all sources
-  source_names,
+  emissions_data=NULL, #data.frame with emissions data for all sources
+  source_names=NULL,
   FGD=NULL,
   species_configuration = "so2_nox_pm_hg",
   emitted.polls = list(so2_nox_pm_hg=c('SO2','NO','NO2','PM15','PM10','PPM25','HG0','RGM'),
@@ -25,11 +25,11 @@ runCalpuff <- function(
   receptors=NULL,
   bgconcs = NULL,
   o3dat = NULL,
-  addparams = addparams,
+  addparams = list(),
   addsubgroups = NULL,
   output_dir=unique(dirname(params_allgrids$METDAT)),
   gis_dir=get_gis_dir(),
-  calpuff_exe='C:/CALPUFF/CALPUFF_v7.2.1_L150618/calpuff_v7.2.1',
+  calpuff_exe='C:/CALPUFF/CALPUFF_v7.2.1_L150618/calpuff_v7.2.1.exe',
   only_make_additional_files=F,
   calpuff_template = list(so2_nox_pm_hg=system.file("extdata", "CALPUFF_7.0_template_Hg.INP", package="creapuff"),
                           so2_nox_pm=system.file("extdata", "CALPUFF_7.0_template.INP", package="creapuff"))[[species_configuration]]
@@ -60,74 +60,79 @@ runCalpuff <- function(
   out_files$dir <- output_dir
   
   calpuff_dir <- dirname(calpuff_exe)
-
-  #default Hg speciation in %, FGD flue gas desulfurization (FGD) systems
-  "FGD  HG0 RGM Hgp
+  
+  if(!is.null(emissions_data)) {
+    #default Hg speciation in %, FGD flue gas desulfurization (FGD) systems
+    "FGD  HG0 RGM Hgp
   F 43.9  54  2.1
   T 74.2  24  1.8" %>% textConnection %>% read.table(header=T) %>% 
-    mutate_if(is.numeric, divide_by, 100) ->
-    hg_species
-  
-  #rename columns to use default variable names
-  sources %<>% rename(Stack.height=contains("Stack height"), 
-                      velocity=contains("velocity"),
-                      diameter = contains("Diameter"),
-                      exit.temp = contains("Temperature"))
-  
-  inpfiles_created <- character(0)
-  
-  inpfiles_created <- list()
-  runsources_out=list()
-  pm10fraction=list()
-  
-  sources %>% to_spdf() %<>% spTransform(targetcrs) -> runsources
-  
-  runsources %>% coordinates() %>% data.frame() %>% 
-    set_names(c('UTMx', 'UTMy')) %>% data.frame(runsources@data, .) -> runsources@data
-  
-  if(is.null(runsources$base.elevation..msl))
-    runsources$base.elevation..msl <- get_plant_elev(runsources,
-                                                     dir=output_dir,
-                                                     out_files=out_files)
-  
-  for(emission_col in c('SO2_tpa', 'NOx_tpa', 'PM_tpa', 'Hg_kgpa', emitted.polls, 'downwash'))
-    if(is.null(runsources[[emission_col]])) runsources[[emission_col]]=0
-  
-  if(is.null(runsources$SO2)) runsources@data %<>% mutate(SO2 = SO2_tpa)
-  
-  if(is.null(runsources$NO)) runsources@data %<>% mutate(NO  = NOx_tpa * .95 * 30/46,
-                                                         NO2 = NOx_tpa * .05)
-  
-  if(is.null(runsources$PPM25)) runsources@data %<>% mutate(PM15 = PM_tpa * 26/80,
-                                                            PM10 = PM_tpa * 30/80,
-                                                            PPM25 = PM_tpa * 24/80)
-  
-  if(is.null(runsources$RGM)) 
-    runsources@data %<>% mutate(HG0 = Hg_kgpa * hg_species$HG0[match(runsources$FGD, hg_species$FGD)], 
-                                RGM = Hg_kgpa * hg_species$RGM[match(runsources$FGD, hg_species$FGD)],
-                                Hgp = Hg_kgpa * hg_species$Hgp[match(runsources$FGD, hg_species$FGD)])
-  
-  runsources$exit.temp %<>% (function(x) x+ifelse(x < 273, 273.15, 0))
-  
-  runsources@data %>% ungroup %>% 
-    select(UTMx, UTMy, Stack.height, base.elevation..msl, diameter, velocity, exit.temp, downwash,
-           all_of(emitted.polls)) -> runsources2
-  
-  runsources2 %>% 
-    mutate_all(signif, 6) %>% mutate_all(format, nsmall=1) %>% 
-    apply(1, paste, collapse=", ") -> emislines
-  
-  #  Source       X         Y       Stack    Base     Stack    Exit  Exit    Bldg.  Emission
-  #No.     Coordinate Coordinate Height Elevation Diameter  Vel.  Temp.   Dwash   Rates
-  #(km)      (km)       (m)      (m)       (m)  (m/s) (deg. K)         
-  #------   ---------- ---------- ------  ------   -------- ----- -------- ----- --------
+      mutate_if(is.numeric, divide_by, 100) ->
+      hg_species
     
-  sourceLines <- list()
-  for(i in 1:nrow(runsources))
-    sourceLines[[runsources$source.name[i]]] <- c(paste0("! SRCNAM =",runsources$source.name[i],"!"),
-                                                  paste("! X =", emislines[i], "!"),
-                                                  "! ZPLTFM  =       .0 !",
-                                                  "! FMFAC  =      1.0 !   !END!")
+    #rename columns to use default variable names
+    emissions_data %<>% rename(Stack.height=contains("Stack height"), 
+                        velocity=contains("velocity"),
+                        diameter = contains("Diameter"),
+                        exit.temp = contains("Temperature"))
+    
+    inpfiles_created <- character(0)
+    
+    inpfiles_created <- list()
+    runsources_out=list()
+    pm10fraction=list()
+    
+    emissions_data %>% to_spdf() %<>% spTransform(targetcrs) -> runsources
+    
+    runsources$source.name <- source_names
+    runsources %>% coordinates() %>% data.frame() %>% 
+      set_names(c('UTMx', 'UTMy')) %>% data.frame(runsources@data, .) -> runsources@data
+    
+    if(is.null(runsources$base.elevation..msl))
+      runsources$base.elevation..msl <- get_plant_elev(runsources,
+                                                       dir=output_dir,
+                                                       out_files=out_files)
+    
+    for(emission_col in c('SO2_tpa', 'NOx_tpa', 'PM_tpa', 'Hg_kgpa', emitted.polls, 'downwash'))
+      if(is.null(runsources[[emission_col]])) runsources[[emission_col]]=0
+    
+    if(is.null(runsources$SO2)) runsources@data %<>% mutate(SO2 = SO2_tpa)
+    
+    if(is.null(runsources$NO)) runsources@data %<>% mutate(NO  = NOx_tpa * .95 * 30/46,
+                                                           NO2 = NOx_tpa * .05)
+    
+    if(is.null(runsources$PPM25)) runsources@data %<>% mutate(PM15 = PM_tpa * 26/80,
+                                                              PM10 = PM_tpa * 30/80,
+                                                              PPM25 = PM_tpa * 24/80)
+    
+    if(is.null(runsources$RGM)) 
+      runsources@data %<>% mutate(HG0 = Hg_kgpa * hg_species$HG0[match(runsources$FGD, hg_species$FGD)], 
+                                  RGM = Hg_kgpa * hg_species$RGM[match(runsources$FGD, hg_species$FGD)],
+                                  Hgp = Hg_kgpa * hg_species$Hgp[match(runsources$FGD, hg_species$FGD)])
+    
+    runsources$exit.temp %<>% (function(x) x+ifelse(x < 273, 273.15, 0))
+    
+    runsources@data %>% ungroup %>% 
+      select(UTMx, UTMy, Stack.height, base.elevation..msl, diameter, velocity, exit.temp, downwash,
+             all_of(emitted.polls)) -> runsources2
+    
+    runsources2 %>% 
+      mutate_all(signif, 6) %>% mutate_all(format, nsmall=1) %>% 
+      apply(1, paste, collapse=", ") -> emislines
+    
+    #  Source       X         Y       Stack    Base     Stack    Exit  Exit    Bldg.  Emission
+    #No.     Coordinate Coordinate Height Elevation Diameter  Vel.  Temp.   Dwash   Rates
+    #(km)      (km)       (m)      (m)       (m)  (m/s) (deg. K)         
+    #------   ---------- ---------- ------  ------   -------- ----- -------- ----- --------
+    
+    sourceLines <- list()
+    for(i in 1:nrow(runsources))
+      sourceLines[[runsources$source.name[i]]] <- c(paste0("! SRCNAM =",runsources$source.name[i],"!"),
+                                                    paste("! X =", emislines[i], "!"),
+                                                    "! ZPLTFM  =       .0 !",
+                                                    "! FMFAC  =      1.0 !   !END!")
+  }
+
+  
   
   if(is.null(addparams$DATUM)) addparams$DATUM="WGS-84"
   
@@ -138,8 +143,8 @@ runCalpuff <- function(
                    bgconcs = bgconcs,
                    OZONE.DAT = o3dat,
                    sourceLines=sourceLines,
-                   receptors=intopo %>% subset(include) %>% make_topo_rows) ->
-    inpfiles_created[[puffrun]]
+                   receptors=receptors %>% subset(include) %>% make_topo_rows) ->
+    inpfiles_created
   
   pm10fraction = sum(runsources$Hgp) / sum(runsources$PM10)
   
