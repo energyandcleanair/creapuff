0cf466adae36558add9c78bc34c48aed659d1673
lorenzocostantino <lorenzo@energyandcleanair.org>
2021-10-06 09:43:56 +0000

    Full workflow for plotting Chile simulations, using constant emissions.

--- workflow_plotting_chile.R
+++ workflow_plotting_chile.R
@@ -7,43 +7,56 @@ library(readxl)
 
 
 # Parameters ###################################################################
-
 # ============================= Project specific ===============================
+# Reference scenario
+scenario_prefix_ref<- "ScAll"
+
+# Select macro scenario
+scenario_prefix <- "ScAll" ; scenario_description='Coal and Gas FPPs (2019)'
+scenario_prefix <- "ScB" ; scenario_description='Coal and Gas FPPs (SPS-2025)'
+scenario_prefix <- "ScC" ; scenario_description='Gas FPPs (LPS-2040)'
+
 project_dir="G:/projects/chile"        # calpuff_external_data persistent disk (project data)
-output_dir <- file.path(project_dir,"calpuff_suite") # Where to write all generated files
+# project_dir="Z:/projects/chile"        # calpuff_external_data persistent disk (project data)
+output_dir <- file.path(project_dir, "calpuff_suite") # Where to write all generated files
 # output_dir <- "F:/TAPM/Drax/" # Where to write all generated files
-emissions_dir <- file.path(project_dir,"emissions/2019") # Directory where arbitrary-varying emission files are stored
-input_xls <- file.path(emissions_dir,"coordinates.xlsx") # Where plant positions are reported
+emissions_dir <- file.path(project_dir, "emissions/2019") # Directory where arbitrary-varying emission files are stored
+input_xls <- file.path(emissions_dir, paste0("coordinates_",scenario_prefix,".xlsx")) # Where plant positions are reported
+input_xls_ref <- file.path(emissions_dir, paste0("coordinates_",scenario_prefix_ref,".xlsx")) # Where plant positions are reported
 
 # ================================ General =====================================
 gis_dir <- "F:/gis"                         # The folder where we store general GIS data
 # creahia::set_env('gis_dir',"~/GIS/")
 # Sys.setenv(gis_dir="~/GIS/")
-#setwd(get_gis_dir())
-#system("gsutil rsync -r gs://crea-data/gis .")
+# setwd(get_gis_dir())
+# system("gsutil rsync -r gs://crea-data/gis .")
 
 
 # Plots ########################################################################
-
 # Load CALMET parameters
 calmet_result <- readRDS(file.path(output_dir,"calmet_result.RDS" ))
 UTMZ <- calmet_result$params[[01]]$IUTMZN
 UTMH <- calmet_result$params[[01]]$UTMHEM
 
 # List generated tif files
-calpuff_files <- get_calpuff_files(ext=".csv", gasunit = 'ug', dir=output_dir)
+calpuff_files <- get_calpuff_files(ext=paste0(tolower(scenario_prefix),".csv"), gasunit = 'ug', dir=output_dir)
+# calpuff_files <- get_calpuff_files(ext=paste0(tolower(scenario_prefix),".csv"), gasunit = 'ug', dir=output_dir, hg_scaling=1e-3)
 grids = get_grids_calpuff(calpuff_files, UTMZ, UTMH, map_res=1)
-# calpuff_files %>% filter(period=='annual' | !is.na(threshold)) %>% make_tifs(grids = grids)  # Why this filter ??
 
-# Select only chileall
-calpuff_files %>% slice(grep('chileall', calpuff_files$name)) %>% make_tifs(grids = grids)
+# Select data and make tif
+# calpuff_files %>% filter(period=='annual' | !is.na(threshold)) %>% make_tifs(grids = grids)  # Original filtering
+# calpuff_files %<>% slice(grep(tolower(scenario_prefix), calpuff_files$name)) %>% make_tifs(grids = grids)
+calpuff_files %>% make_tifs(grids = grids)
 
-calpuff_files <- get_calpuff_files(ext=".tif", gasunit = 'ug', dir=output_dir)
+# Select tif data 
+calpuff_files <- get_calpuff_files(ext=paste0(tolower(scenario_prefix),".tif"), gasunit = 'ug', dir=output_dir)
+# calpuff_files <- get_calpuff_files(ext=".tif", gasunit = 'ug', dir=output_dir)
 
 # Read list of modeled plants
 # plants <- read_csv('F:/TAPM/Drax/stations.txt')
 # plants <- read_csv(input_xls)
-plants <- read_xlsx(input_xls, sheet='CALPUFF input')
+# plants <- read_xlsx(input_xls, sheet='CALPUFF input')
+plants <- read_xlsx(input_xls_ref, sheet='CALPUFF input')
 
 target_crs = get_utm_proj(UTMZ, UTMH, units = 'km')
 
@@ -53,7 +66,7 @@ plants %<>% rename(x=Long, y=Lat) %>% to_spdf(crs="+proj=longlat +datum=WGS84") 
   spTransform(target_crs)
 
 #function to select appropriate plants to include in each plot
-get_plants <- function(x, scen) {x}
+# get_plants <- function(x, scen) {x}
 
 #function to zip files in case system standard function doesn't work
 zipping_function=function(zipfile, files_to_zip) {
@@ -68,16 +81,33 @@ plot_bb <- plants %>% extent %>% magrittr::add(400)
 cities <- get_cities(plot_bb, grids)
 # cities$pos[cities$name == 'Kingston upon Hull'] <- 4
 
+# I set max values, using colorkeybasis=TRUE
+calpuff_files$k[calpuff_files$period=="annual" & calpuff_files$species=="no2" ]  = 1.3 #2.0
+calpuff_files$k[calpuff_files$period=="annual" & calpuff_files$species=="pm25" ] = 0.9 #
+calpuff_files$k[calpuff_files$period=="annual" & calpuff_files$species=="tpm10" ] = 1.2 #1.25
+calpuff_files$k[calpuff_files$period=="annual" & calpuff_files$species=="so2" ]  = 2.0 #3.0
+calpuff_files$k[calpuff_files$period=="daily"  & calpuff_files$species=="no2" ]  = 13 #30
+calpuff_files$k[calpuff_files$period=="daily"  & calpuff_files$species=="pm25" ] = 9
+calpuff_files$k[calpuff_files$period=="daily"  & calpuff_files$species=="tpm10" ] = 15 #18
+calpuff_files$k[calpuff_files$period=="daily"  & calpuff_files$species=="so2" ]  = 25 #45
+calpuff_files$k[calpuff_files$period=="hourly" & calpuff_files$species=="no2" ]  = 150 #200
+calpuff_files$k[calpuff_files$period=="hourly" & calpuff_files$species=="pm25" ] = 30 #70
+calpuff_files$k[calpuff_files$period=="hourly" & calpuff_files$species=="so2" ]  = 100 #350
+
+# calpuff_files %<>% filter(calpuff_files$species=="no2")
+
 #output plots and exposure results
 plot_results(calpuff_files,
-             scenario_names = 'Chile Power Stations',
+             scenario_names = paste0(scenario_description),
              dir=output_dir, 
              plants=plants,
              cities=cities,
+             colorkeybasis=TRUE,
              # plant_names='Drax',
              # plant_names=plants@data$Plants,
              zipping_function=zipping_function,
-             #outputs=c('png','expPop'),
+             filename_suffix=paste0("_",scenario_prefix),
+             outputs=c('png','expPop'),
 )
 
 browser()
