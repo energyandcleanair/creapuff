527bdaa14290718cac4aff4e6835ff9774970e30
laurimyllyvirta <laurimyllyvirta@yahoo.com>
2023-06-08 12:53:54 +0300

    workflow to generate HYSPLIT input data for plants around Jakarta

---
+++ project_workflows/indonesia_iesr_hysplit_inputs.R
@@ -0,0 +1,82 @@
+require(raster)
+require(tidyverse)
+require(magrittr)
+require(lubridate)
+require(readxl)
+require(creahelpers)
+require(sf)
+
+source('project_workflows/read_IESR_emissions.R')
+
+emissions_dir <- 'G:/Shared drives/CREA-HIA/Projects/Indonesia_JETP'
+readRDS(file.path(emissions_dir, 'indonesia_iesr_emission_pathways v2.RDS')) -> emis
+
+c('emissions inputs, with clustering.csv', 'emissions inputs, with clustering, additions.csv') %>% 
+  file.path(emissions_dir, .) %>%
+  lapply(read_csv) %>% bind_rows() -> stackdata
+
+
+loc <- tibble(lon=106.8456, lat=-6.2088) %>% to_spdf() %>% st_as_sf()
+
+emis %>% filter(year==2022, scenario=='BAU', Status=='operating', pollutant %in% c('SOx', 'NOx', 'PM')) ->
+  current_emis
+
+require(sf)
+current_emis %>% cluster(5) -> current_emis$cluster
+
+stack_regex <- 'Height|Diameter|Temp|Velocity'
+stackdata %>% select(CFPP.name, matches(stack_regex)) %>%
+  right_join(current_emis) -> current_emis
+
+pm25_potential = tibble(pollutant = c('SOx', 'NOx', 'PM'),
+                        pm25_potential = c(132/64, 80/46, 24/80))
+
+current_emis %>% 
+  left_join(plant_names) %>%
+  group_by(cluster) %>%
+  left_join(pm25_potential) %>%
+  summarise(emissions_t = sum(emissions_t*pm25_potential),
+            across(c(Latitude, Longitude, matches(stack_regex)), ~mean(.x, na.rm=T)),
+            plants=paste(unique(plant_name), collapse='; '),
+            plant_units=paste(CFPP.name, collapse='; ')) ->
+  emis_bycluster
+
+#https://web.iitd.ac.in/~arunku/files/CEL795_Y13/AirPollutionExamples2.pdf
+#Holland's formula
+effective_release_height <- function(flue_velocity_ms,
+                                     stack_diameter_m,
+                                     wind_speed_ms,
+                                     pressure_kPa=101,
+                                     flue_temperature_K,
+                                     air_temperature_K) {
+  (flue_velocity_ms/wind_speed_ms) *
+    (1.5 + (2.68e-2*pressure_kPa) * 
+       (flue_temperature_K / air_temperature_K) / 
+       air_temperature_K * 
+       stack_diameter_m)
+}
+
+emis_bycluster %>% to_spdf() %>% st_as_sf() %>% st_distance(loc) -> emis_bycluster$distance_to_loc
+
+emis_bycluster %<>% mutate(release_height = Height..m. +
+                             effective_release_height(flue_velocity_ms=Velocity..m.s.,
+                                                      stack_diameter_m=Diameter..m.,
+                                                      wind_speed_ms=4,
+                                                      flue_temperature_K=Exit.Temp....+273.15,
+                                                      air_temperature_K=27))
+
+emis_bycluster %>% filter(distance_to_loc<units::set_units(300, 'km'),
+                          distance_to_loc<units::set_units(50, 'km') | emissions_t > 2000,
+                          distance_to_loc<units::set_units(100, 'km') | emissions_t>10000) ->
+  selected_clusters
+
+
+require(ggspatial)
+ggplot(selected_clusters) + 
+  geom_point(aes(Longitude, Latitude, size=emissions_t)) +
+  layer_spatial(loc, color='red')
+
+selected_clusters %>% 
+  mutate(plants=ifelse(grepl('Suralaya', plants), 'Suralaya', plants)) %>%
+  select(plants, plant_units, Latitude, Longitude, release_height, emissions_t) %>%
+  write_csv(file.path(emissions_dir, 'plants_around_Jakarta_for_HYSPLIT.csv'))
